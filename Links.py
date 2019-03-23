101640297df8a0421fadb14e026837030700d255
pranayknight <pranayknight94@gmail.com>
2019-03-23 20:49:51 +0530

    Added New progs

--- Links.py
+++ Links.py
@@ -4,10 +4,12 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Firefox(executable_path="/home/pranay/Drivers/geckodriver-v0.24.0-linux64/geckodriver")
 driver.get("http://newtours.demoaut.com/")
 
+# Printing the no. of links present in the page
 links = driver.find_elements(By.TAG_NAME, "a")
 
 print("No. of links ", len(links))   # Print how many links present in a page
 
+# Printing the content of the links
 for link in links:
     print(link.text)
 
